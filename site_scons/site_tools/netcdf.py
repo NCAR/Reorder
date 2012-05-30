df469d60945415881e397874db8b8454980eaccf
Chris Burghart <burghart@ucar.edu>
2012-05-30 17:23:41 +0000

    Copied (with slight modification) the latest netcdf.py tool from the EOL Subversion repository. We need the tests which add HDF5 libraries when linking if necessary.
    git-svn-id: https://proxy.subversion.ucar.edu/Reorder/trunk@68 32a602d7-ebe0-62b3-8b7b-f5441eef952f

--- site_scons/site_tools/netcdf.py
+++ site_scons/site_tools/netcdf.py
@@ -1,7 +1,32 @@
-import os,os.path
+
+import os, os.path
+import string
+import SCons
+
+netcdf_headers = string.split("""
+ncvalues.h netcdf.h netcdf.hh netcdfcpp.h
+""")
+
+headers = [ os.path.join("$OPT_PREFIX","include",f)
+            for f in netcdf_headers ]
+
+# We extend the standard netcdf installation slightly by also copying
+# the headers into a netcdf subdirectory, so headers can be qualified
+# with a netcdf/ path when included.  Aeros does that, for example.
+
+headers.extend ([ os.path.join("$OPT_PREFIX","include","netcdf",f)
+                  for f in netcdf_headers ])
+libs = string.split("""
+$OPT_PREFIX/lib/libnetcdf.a
+$OPT_PREFIX/lib/libnetcdf_c++.a
+""")
+
 # Try for eol_scons.package.Package, which will allow us to build netCDF
 # from source if necessary.  If we don't find it, do stuff to disable 
 # build-from-source.
+
+netcdf_actions = None
+
 try:
     from eol_scons.package import Package
     from eol_scons.chdir import MkdirIfMissing
@@ -27,31 +52,9 @@ except ImportError:
         def checkBuild(self, env):
             pass
     # empty command set since we won't build from source
-    netcdf_actions = None
-
-import string
-import SCons
 
 # Note that netcdf.inc has been left out of this list, since this
 # current setup does not install it.
-
-netcdf_headers = string.split("""
-ncvalues.h netcdf.h netcdf.hh netcdfcpp.h
-""")
-
-headers = [ os.path.join("$OPT_PREFIX","include",f)
-            for f in netcdf_headers ]
-
-# We extend the standard netcdf installation slightly by also copying
-# the headers into a netcdf subdirectory, so headers can be qualified
-# with a netcdf/ path when included.  Aeros does that, for example.
-
-headers.extend ([ os.path.join("$OPT_PREFIX","include","netcdf",f)
-                  for f in netcdf_headers ])
-libs = string.split("""
-$OPT_PREFIX/lib/libnetcdf.a
-$OPT_PREFIX/lib/libnetcdf_c++.a
-""")
 
 class NetcdfPackage(Package):
 
@@ -59,9 +62,22 @@ class NetcdfPackage(Package):
         dpf="ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-3.6.2.tar.gz"
         Package.__init__(self, "NETCDF", "INSTALL",
                          netcdf_actions, libs + headers, dpf)
+        self.settings = {}
 
     def require(self, env):
         "Need to add both c and c++ libraries to the environment."
+        # The netcdf tool avails itself of the settings in the
+        # prefixoptions tool, so make sure it gets required first.
+        try:
+            env.Require('prefixoptions')
+        except:
+            pass
+
+        if not self.settings:
+            self.calculate_settings(env)
+        self.apply_settings(env)
+
+    def calculate_settings(self, env):
         Package.checkBuild(self, env)
         if env.has_key('OPT_PREFIX'):
             prefix = env['OPT_PREFIX']
@@ -75,15 +91,56 @@ class NetcdfPackage(Package):
                      "/usr/include/netcdf" ]
         header = env.FindFile("netcdf.h", incpaths)
         if header:
-            env.AppendUnique(CPPPATH=header.get_dir().get_abspath())
+            self.settings['CPPPATH'] = [ header.get_dir().get_abspath() ]
         if self.building:
-            env.Append(LIBS=env.File(libs[0]))
-            env.Append(LIBS=env.File(libs[1]))
+            self.settings['LIBS'] = [ env.File(libs[0]), env.File(libs[1]) ]
         else:
-            env.AppendUnique(LIBPATH=[os.path.join(prefix,'lib')])
-            env.AppendUnique(RPATH=[os.path.join(prefix,'lib')])
-            env.AppendUnique(LIBPATH=['/usr/lib/netcdf-3'])
-            env.Append(LIBS=['netcdf_c++', 'netcdf'])
+            self.settings['LIBPATH'] = [os.path.join(prefix,'lib')]
+            self.settings['RPATH'] = [os.path.join(prefix,'lib')]
+            self.settings['LIBPATH'].append('/usr/lib/netcdf-3')
+
+            # Now try to check whether the HDF libraries are needed
+            # explicitly when linking with netcdf.  Use a cloned
+            # Environment so Configure does not modify the original
+            # Environment.  Also, reset the LIBS so that libraries in
+            # the original Environment do not affect the linking.  All
+            # the library link check needs is the netcdf-related
+            # libraries.
+
+            clone = env.Clone()
+            libs = ['netcdf_c++', 'netcdf']
+            clone.Replace(LIBS=libs)
+            conf = clone.Configure()
+            if not conf.CheckLib('netcdf'):
+                libs.append(['hdf5_hl', 'hdf5', 'bz2'])
+                clone.Replace(LIBS=libs)
+                if not conf.CheckLib('netcdf'):
+                    msg = "Failed to link to netcdf both with and without"
+                    msg += " explicit HDF libraries.  Check config.log."
+                    raise SCons.Errors.StopError, msg
+            self.settings['LIBS'] = libs
+            conf.Finish()
+
+    def apply_settings(self, env):
+        if self.settings.has_key('CPPPATH'):
+            env.AppendUnique(CPPPATH=self.settings['CPPPATH'])
+
+        env.Append(LIBS=self.settings['LIBS'])
+        if not self.building:
+            env.AppendUnique(LIBPATH=self.settings['LIBPATH'])
+            env.AppendUnique(RPATH=self.settings['RPATH'])
+
+
+
+# Background on Configure check for netcdf linking: The first attempt
+# directly used the Environment passed in.  That works as long as the
+# Environment does not already contain dependencies (such as internal
+# project libraries) which break the linking.  The other option was to
+# create a brand new Environment.  However, if this tool is a global
+# tool, then there will be infinite recursion trying to create the new
+# Environment.  So the current approach clones the Environment, but
+# then resets the LIBS list on the assumption that none of those
+# dependencies are needed to link with netcdf.
 
 netcdf_package = NetcdfPackage()
 
